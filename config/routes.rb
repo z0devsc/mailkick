fc0cd0862a75a907c0a1636d71abe02661c672d1
z0devsc <102047757+z0devsc@users.noreply.github.com>
2022-07-10 20:49:16 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  unless respond_to?(:has_named_route?) && has_named_route?("mailkick")
-    mount Mailkick::Engine => "/mailkick" if Mailkick.mount
-  end
+  mount Mailkick::Engine => "/mailkick"
 end
 
 Mailkick::Engine.routes.draw do
